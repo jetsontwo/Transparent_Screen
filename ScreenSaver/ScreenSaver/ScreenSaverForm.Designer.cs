253df975b401776b4fb289034df736f205c27d42
jetsontwo <jetsontwo@gmail.com>
2018-01-14 22:53:09 -0800

    Added Script to Make it a ScreenSaver
    Added the visual studios project that makes the game a screen saver.

---
+++ ScreenSaver/ScreenSaver/ScreenSaverForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ScreenSaver
+{
+    partial class ScreenSaverForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(16, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "GHello";
+            // 
+            // ScreenSaverForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ScreenSaverForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ScreenSaverForm_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ScreenSaverForm_KeyPress);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ScreenSaverForm_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ScreenSaverForm_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ScreenSaver/ScreenSaver/ScreenSaverForm.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace ScreenSaver
+{
+    //Code taken from https://www.harding.edu/fmccown/screensaver/screensaver.html
+
+    public partial class ScreenSaverForm : Form
+    {
+
+        //[DllImport("user32.dll")]
+        //static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        //[DllImport("user32.dll")]
+        //static extern int SetWindowLong(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+        //[DllImport("user32.dll", SetLastError = true)]
+        //static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        //[DllImport("user32.dll")]
+        //static extern bool GetClientRect(IntPtr hWnd, out Rectangle lpRect);
+
+
+        private Point mouseLocation;
+        private bool previewMode = false;
+
+
+        //Basic Constructor
+        public ScreenSaverForm(Rectangle Bounds)
+        {
+            InitializeComponent();
+            this.Bounds = Bounds;
+        }
+
+        //Preview Constructor
+        //public ScreenSaverForm(IntPtr PreviewWndHandle)
+        //{
+        //    InitializeComponent();
+
+        //    // Set the preview window as the parent of this window
+        //    SetParent(this.Handle, PreviewWndHandle);
+
+        //    // Make this a child window so it will close when the parent dialog closes
+        //    // GWL_STYLE = -16, WS_CHILD = 0x40000000
+        //    SetWindowLong(this.Handle, -16, new IntPtr(GetWindowLong(this.Handle, -16) | 0x40000000));
+
+        //    // Place our window inside the parent
+        //    Rectangle ParentRect;
+        //    GetClientRect(PreviewWndHandle, out ParentRect);
+        //    Size = ParentRect.Size;
+        //    Location = new Point(0, 0);
+
+        //    // Make text smaller
+        //    textLabel.Font = new System.Drawing.Font("Arial", 6);
+
+        //    previewMode = true;
+        //}
+
+
+
+
+
+
+        //Event Handlers
+
+        private void ScreenSaverForm_Load(object sender, EventArgs e)
+        {
+            Cursor.Hide();
+            TopMost = true;
+        }
+
+        private void ScreenSaverForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!mouseLocation.IsEmpty && !previewMode)
+            {
+                // Terminate if mouse is moved a significant distance
+                if (Math.Abs(mouseLocation.X - e.X) > 5 ||
+                    Math.Abs(mouseLocation.Y - e.Y) > 5)
+                    Application.Exit();
+            }
+
+            // Update current mouse location
+            mouseLocation = e.Location;
+
+        }
+
+        private void ScreenSaverForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!previewMode)
+                Application.Exit();
+
+        }
+
+        private void ScreenSaverForm_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!previewMode)
+                Application.Exit();
+
+        }
+    }
+}
