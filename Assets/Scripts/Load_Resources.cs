cdaaa14d12cd1ab1945cb741e75a9d9b613f388d
jetsontwo <jetsontwo@gmail.com>
2018-01-14 23:45:37 -0800

    Villagers now have Names
    Cause why not. I'll probably add a button to hid the names in the future
    when implementing event hotkeys.

--- Assets/Scripts/Load_Resources.cs
+++ Assets/Scripts/Load_Resources.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Load_Resources : MonoBehaviour {
 
@@ -11,6 +12,16 @@ public class Load_Resources : MonoBehaviour {
     public float timer;
     public int num_trees, num_berries, num_stone;
     public GameObject tree, berry, stone, Material_Parent;
+
+    private string[] names = new string[]{
+        "James", "John", "Robert","Michael","William","David","Richard","Joseph","Thomas","Charles","Christopher","Daniel","Matthew","Anthony","Donald","Mark","Paul","Steven","Andrew","Kenneth","George","Joshua","Kevin","Brian","Edward","Ronald","Timothy","Jason",
+        "Jeffrey","Ryan","Gary","Jacob","Nicholas","Eric","Stephen","Jonathan","Larry","Justin","Scott","Frank","Brandon","Raymond","Gregory","Benjamin","Samuel","Patrick","Alexander","Jack","Dennis","Jerry","Tyler","Aaron","Henry","Douglas","Jose","Peter","Adam",
+        "Zachary","Nathan","Walter","Harold","Kyle","Carl","Arthur","Gerald","Roger","Keith","Jeremy","Terry","Lawrence","Sean","Christian","Albert","Joe","Ethan","Austin","Jesse","Willie","Billy","Bryan","Bruce","Jordan","Ralph","Roy","Noah","Dylan","Eugene","Wayne",
+        "Alan","Juan","Louis","Russell","Gabriel","Randy","Philip","Harry","Vincent","Bobby","Johnny","Logan","Mary","Patricia","Jennifer","Elizabeth","Linda","Barbara","Susan","Jessica","Margaret","Sarah","Karen","Nancy","Betty","Lisa","Dorothy","Sandra","Ashley",
+        "Kimberly","Donna","Carol","Michelle","Emily","Amanda","Helen","Melissa","Deborah","Stephanie","Laura","Rebecca","Sharon","Cynthia","Kathleen","Amy","Shirley","Anna","Angela","Ruth","Brenda","Pamela","Nicole","Katherine","Virginia","Catherine","Christine",
+        "Samantha","Debra","Janet","Rachel","Carolyn","Emma","Maria","Heather","Diane","Julie","Joyce","Evelyn","Frances","Joan","Christina","Kelly","Victoria","Lauren","Martha","Judith","Cheryl","Megan","Andrea","Ann","Alice","Jean","Doris","Jacqueline","Kathryn",
+        "Hannah","Olivia","Gloria","Marie","Teresa","Sara","Janice","Julia","Grace","Judy","Theresa","Rose","Beverly","Denise","Marilyn","Amber","Madison","Danielle","Brittany","Diana","Abigail","Jane","Natalie","Lori","Tiffany","Alexis","Kayla", "Ittai", "Mayan",
+        "Claudia"};
 
     // Use this for initialization
     void Start()
@@ -51,7 +62,9 @@ public class Load_Resources : MonoBehaviour {
         for(int i =0; i < count; ++i)
         {
             villagers[i].transform.position = new Vector2(0, 0);
+            villagers[i].transform.parent = transform;
             villagers[i].SetActive(true);
+            villagers[i].transform.GetChild(0).GetComponent<Text>().text = names[Random.Range(0, names.Length)];
             yield return new WaitForSeconds(timer);
         }
     }
